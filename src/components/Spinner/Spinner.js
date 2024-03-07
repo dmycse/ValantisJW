78aec0006713a21f406889c9a10b8f43a99e99b1
dmycse <dmycse@gmail.com>
2024-03-07 19:23:47 +0200

    Create Spinner component

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,12 +1,27 @@
-
+import { SPINNER } from '../../constants/htmlNodeElements';
+import SpinnerImg from '../../../public/icons/spinner.svg';
 import './Spinner.css';
 
 
 class Spinner {
 
-  
+  static createSpinnerHTML() {
+    let spinnerHTML = `
+      <div class="spinner__container">
+        <img class="spinner__img" src="${SpinnerImg}">
+        <p class="spinner__message">Идёт поиск и загрузка данных..... Подождите</p>
+      </div>
+      `;
+
+    SPINNER.innerHTML = spinnerHTML;
+  }
+
+  removeSpinner() {
+    SPINNER.innerHTML = '';
+  }
+
   render() {
-    
+    Spinner.createSpinnerHTML();
   }
   
 }
