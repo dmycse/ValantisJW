745ddc9478ab5db672e8d4ff1c7fb7153e24c36f
dmycse <dmycse@gmail.com>
2024-03-07 19:28:33 +0200

    Create NoDataPage component

--- src/components/NoDataPage/NoDataPage.js
+++ src/components/NoDataPage/NoDataPage.js
@@ -1,11 +1,32 @@
-
+import { FILTER } from "../../constants/htmlNodeElements";
 import "./NoDataPage.css";
 
 
 class NoDataPage {
 
-  render() {
+  removeNoDataHTML() {
+    FILTER.lastElementChild.remove();
+  }
+
+  static createNoDataHTML() {
+    let noDataContainer = document.createElement('div');
+    noDataContainer.classList.add('nodata__container');
+  
+    let noDataHTML = `
+        <p class="nodata__message">
+          По Вашему запросу данные не найдены!
+        </p>
+        <p class="nodata__message">
+          Измените параметры запроса и повторите поиск.
+        </p>
+    `;
     
+    noDataContainer.innerHTML = noDataHTML;
+    FILTER.insertAdjacentElement('beforeend', noDataContainer);
+  }
+
+  render() {
+    NoDataPage.createNoDataHTML();
   }
 }
 
